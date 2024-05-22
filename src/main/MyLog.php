a22f3cbdcf0c2294ad22b8b90977e857c4933549
2018240448 <2018240448@qq.com>
2024-05-22 14:35:56 +0800

    新增日志封装 文件存储路径修改

--- src/main/MyLog.php
+++ src/main/MyLog.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Zishu\Myextend\main;
 
 /***
@@ -6,8 +7,8 @@ namespace Zishu\Myextend\main;
  * 操作(使用)日志 写入文件 读取 删除
  * @param type 可定义为不同的日志路径 如 admin
  */
-
-class MyLog{
+class MyLog
+{
 
     private $baseDir; //基本路径
     private $subDir; //子目录
@@ -17,9 +18,9 @@ class MyLog{
      * 构造
      * $type  admin  index  api 等
      */
-    public function __construct($type='admin')
+    public function __construct($type = 'admin')
     {
-        $this->baseDir = $_SERVER['DOCUMENT_ROOT']."/use_log/".$type; //基本路径
+        $this->baseDir = $_SERVER['DOCUMENT_ROOT'] . "/use_log/" . $type; //基本路径
         $this->subDir = date("Ym");  //子目录 年月份为名
         $this->fileName = date("Ymd") . ".txt"; //文件名称及后缀 年月份天为名
     }
@@ -29,21 +30,21 @@ class MyLog{
      * @param $user_id 用户id
      * 可记录
      */
-public function log($user_id)
+    public function log($user_id)
     {
         //记录数据组成
         $content = [
-            "userId"=>$user_id,//请求用户id
-            "ip"=> $_SERVER['REMOTE_ADDR'],//访问ip
-            "time"=>date('Y-m-d H:i:s',time()),//请求时间
-            "uri"=>$_SERVER['REQUEST_URI'],//请求的接口
-            "ua"=>$_SERVER['HTTP_USER_AGENT'], //用户代理 浏览器
-            "method"=>$_SERVER['REQUEST_METHOD'],//请求方式
+            "userId" => $user_id,//请求用户id
+            "ip" => $_SERVER['REMOTE_ADDR'],//访问ip
+            "time" => date('Y-m-d H:i:s', time()),//请求时间
+            "uri" => $_SERVER['REQUEST_URI'],//请求的接口
+            "ua" => $_SERVER['HTTP_USER_AGENT'], //用户代理 浏览器
+            "method" => $_SERVER['REQUEST_METHOD'],//请求方式
         ];
 
-        if($content['method'] == 'GET'){
+        if ($content['method'] == 'GET') {
             $content['param'] = $_GET; //请求参数
-        }else{
+        } else {
             $content['param'] = $_POST; //请求参数
         }
 
@@ -54,7 +55,7 @@ public function log($user_id)
         }
 
         $filePath = $fileDir . "/" . $this->fileName; //文件的完整路径
-        $logContent =json_encode($content)."\r\n"; // 转为json_encode 并 每条记录换行
+        $logContent = json_encode($content) . "\r\n"; // 转为json_encode 并 每条记录换行
         error_log($logContent, 3, $filePath); //写入文件中
     }
 
@@ -62,20 +63,21 @@ public function log($user_id)
      * 获取并解析日志文件
      * @param $time 为时间戳
      */
-public function get_log($time){
-        $list=[];
-        $sub = date('Ym',$time);
-        $name = date('Ymd',$time).".txt";
-        $file= $this->baseDir . "/" . $sub. "/" .$name;
+    public function get_log($time)
+    {
+        $list = [];
+        $sub = date('Ym', $time);
+        $name = date('Ymd', $time) . ".txt";
+        $file = $this->baseDir . "/" . $sub . "/" . $name;
 
         //是否存在
-        if(file_exists($file)){
+        if (file_exists($file)) {
             $data = file_get_contents($file);
             //内容以换行符分割为数组
-            $data=explode("\r\n",$data);
-            if(is_array($data)){
-                foreach ($data as $key=>$vs){
-                    if(!empty($vs)) {
+            $data = explode("\r\n", $data);
+            if (is_array($data)) {
+                foreach ($data as $key => $vs) {
+                    if (!empty($vs)) {
                         $list[] = json_decode($vs, true);
                     }
                 }
@@ -88,14 +90,15 @@ public function get_log($time){
      * 删除日志
      * @param $time 为文件名
      */
-   public function del_log($time){
+    public function del_log($time)
+    {
 
-        $sub = date('Ym',$time);
-        $name = date('Ymd',$time).".txt";
-        $file= $this->baseDir . "/" . $sub. "/" .$name;
+        $sub = date('Ym', $time);
+        $name = date('Ymd', $time) . ".txt";
+        $file = $this->baseDir . "/" . $sub . "/" . $name;
 
         //是否存在
-        if(file_exists($file)){
+        if (file_exists($file)) {
             unlink($file); //删除文件
         }
         return true;
