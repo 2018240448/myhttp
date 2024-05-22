bbeece150c1ec54a2120a99099deb2b3a5436be0
2018240448 <2018240448@qq.com>
2024-05-22 09:57:12 +0800

    新增日志封装，文件说明

--- README.md
+++ README.md
@@ -1,8 +1,89 @@
-# MyPHPExtend （个人PHP扩展）
-个人PHP扩展  
-① http curl请求封装,类名称 MyHttp;  
-② 请求参数加密解密封装,类名称 MyParams;  
-③ redis常用函数封装,redis;  
-  
-  
-  composer require zishu/myextend:^1.0 
+## MyPHPExtend （个人PHP扩展）
+####个人PHP扩展  
+#####① http curl请求封装,类名称 MyHttp;  
+#####② 请求参数加密解密封装,类名称 MyParams;  
+#####③ redis常用函数封装,redis;  
+#####④ 日志记录类;
+
+`php:=>8.0`
+
+ ` composer require zishu/myextend:^1.0 `
+
+## 使用  
+###1. MyHttp类使用(http请求类)
+
+```
+//get实例引用
+\Zishu\Myextend\main\MyHttp::get('url',[]);
+
+//post实例引用
+\Zishu\Myextend\main\MyHttp::post('url',[]);
+```
+
+###2. MyLog类使用(日志记录类)
+
+```
+//PHP 项目 操作日志记录
+$log = new \Zishu\Myextend\main\MyLog('admin'); //实例化 , 参数为类型.admin即项目应用模块的类型
+
+//日志记录
+$userId = 1;
+$log->log($userId);
+
+//获取日志
+$time = time(); //可以选中的时间
+$log->get_log($time);
+
+//删除日志
+$time = time(); //可以选中的时间
+$log->del_log($time);
+```
+
+###3. MyParams类使用(参数加密解密类)
+
+```
+//加密
+$params=[];//需要加密的参数数组
+$key = '';//动态密钥 或者 固定配置的常量
+$params = new \Zishu\Myextend\main\MyParams();
+$params->reqEncode($params,$key); //解密
+```
+
+
+```   
+//解密
+$params=[
+  'key'=>'',//客户端请求返回，或者 固定配置的常量
+  'data'=>'',//客户端请求的加密参数
+];//参数
+$params = new \Zishu\Myextend\main\MyParams();
+$params->reqDecode($params); //解密
+```
+
+###4. redis类（分为PHP原生项目PhpRedis类和 ThinkPHP项目的TpRedis类）
+
+```
+//ThinkPHP 项目
+$redis = new \Zishu\Myextend\main\redis\TpRedis(); //实例化
+
+//加入缓存
+$data = 'hello redis';
+$redis->set('redis_name',$data);
+        
+// 获取缓存
+$data = $redis->get('redis_name');
+```
+
+```
+//ThinkPHP 项目
+$redis = new \Zishu\Myextend\main\redis\TpRedis(); //实例化
+
+//加入缓存
+$data = 'hello redis';
+$redis->set('redis_name',$data);
+        
+// 获取缓存
+$data = $redis->get('redis_name');
+```
+
+###5. 继续增加中...
---
+++ src/main/MyLog.php
@@ -0,0 +1,107 @@
+<?php
+namespace Zishu\Myextend\main;
+
+/***
+ * 日志类 存储在本地工作目录下路径下
+ * 操作(使用)日志 写入文件 读取 删除
+ * @param type 可定义为不同的日志路径 如 admin
+ */
+
+class MyLog{
+
+    private $baseDir; //基本路径
+    private $subDir; //子目录
+    private $fileName; //文件名称及后缀
+
+    /**
+     * 构造
+     * $type  admin  index  api 等
+     */
+    public function __construct($type='admin')
+    {
+        define("DS",DIRECTORY_SEPARATOR); //定义常量路径
+//        define("ROOT",getcwd().DS); //当前工作目录
+//        define("ROOT", app()->getRootPath()); //ThinkPHP 获取当前运行脚本所在的文档根目录
+        define("ROOT",$_SERVER['DOCUMENT_ROOT'].DS); //获取当前运行脚本所在的文档根目录 (入口文件下的目录)
+        $this->baseDir = ROOT."use_log/".$type; //基本路径
+        $this->subDir = date("Ym");  //子目录 年月份为名
+        $this->fileName = date("Ymd") . ".txt"; //文件名称及后缀 年月份天为名
+    }
+
+    /**
+     * 记录日志 日志内容
+     * @param $user_id 用户id
+     * 可记录
+     */
+public function log($user_id)
+    {
+        //记录数据组成
+        $content = [
+            "userId"=>$user_id,//请求用户id
+            "ip"=> $_SERVER['REMOTE_ADDR'],//访问ip
+            "time"=>date('Y-m-d H:i:s',time()),//请求时间
+            "uri"=>$_SERVER['REQUEST_URI'],//请求的接口
+            "ua"=>$_SERVER['HTTP_USER_AGENT'], //用户代理 浏览器
+            "method"=>$_SERVER['REQUEST_METHOD'],//请求方式
+        ];
+
+        if($content['method'] == 'GET'){
+            $content['param'] = $_GET; //请求参数
+        }else{
+            $content['param'] = $_POST; //请求参数
+        }
+
+        //是否存在文件 不存在则新增
+        $fileDir = $this->baseDir . "/" . $this->subDir;
+        if (!is_dir($fileDir)) {
+            mkdir($fileDir, 0777, true);
+        }
+
+        $filePath = $fileDir . "/" . $this->fileName; //文件的完整路径
+        $logContent =json_encode($content)."\r\n"; // 转为json_encode 并 每条记录换行
+        error_log($logContent, 3, $filePath); //写入文件中
+    }
+
+    /**
+     * 获取并解析日志文件
+     * @param $time 为时间戳
+     */
+public function get_log($time){
+        $list=[];
+        $sub = date('Ym',$time);
+        $name = date('Ymd',$time).".txt";
+        $file= $this->baseDir . "/" . $sub. "/" .$name;
+
+        //是否存在
+        if(file_exists($file)){
+            $data = file_get_contents($file);
+            //内容以换行符分割为数组
+            $data=explode("\r\n",$data);
+            if(is_array($data)){
+                foreach ($data as $key=>$vs){
+                    if(!empty($vs)) {
+                        $list[] = json_decode($vs, true);
+                    }
+                }
+            }
+        }
+        return $list;
+    }
+
+    /**
+     * 删除日志
+     * @param $time 为文件名
+     */
+public function del_log($time){
+
+        $sub = date('Ym',$time);
+        $name = date('Ymd',$time).".txt";
+        $file= $this->baseDir . "/" . $sub. "/" .$name;
+
+        //是否存在
+        if(file_exists($file)){
+            unlink($file); //删除文件
+        }
+        return true;
+    }
+}
